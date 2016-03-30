17824920cb84b7de170082a7f17d1e20483aea7c
zhlf8888 <904154180@qq.com>
2016-03-30 14:22:42 +0800

    Tags

---
+++ Tags.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags {
+    public static string Players = "Players";
+    public static string MainLight = "MainLight";
+    public static string AlarmLight = "AlarmLight";
+    public static string GameController = "GameController";
+    public static string Fader = "Fader";
+    public static string Enemy = "Enemy";
+    public static string Siren = "Siren";
+}
