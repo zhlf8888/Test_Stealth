4bc3a2195dcfdb5670b6d5833ae35bd518c97a68
zhlf8888 <904154180@qq.com>
2016-04-24 20:06:55 +0800

    nothing

--- AlarmLight.cs
+++ AlarmLight.cs
@@ -30,13 +30,11 @@ public class AlarmLight : MonoBehaviour
         }
     }
 
-    // Use this for initialization
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (alarmOn)
